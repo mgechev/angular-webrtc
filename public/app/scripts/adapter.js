feb9275c01dbe06c6d32111e8cc67bb6429c58da
mgechev <mgechev@gmail.com>
2014-12-22 18:38:41 +0200

    Make the adapter works in firefox

--- public/app/scripts/adapter.js
+++ public/app/scripts/adapter.js
@@ -1,4 +1,6 @@
 window.RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 window.URL = window.URL || window.mozURL || window.webkitURL;
 window.navigator.getUserMedia = window.navigator.getUserMedia || window.navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
 
